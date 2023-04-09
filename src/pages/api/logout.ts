8e3de45b73b5bb94b363a2799f688a849f2fd5e6
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-04-10 00:15:28 +0900

    [update] vercel の Cookie のキャッシュが効かないようにするための明示的に削除する処理を追加

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,5 +1,6 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
 import { sessionOptions } from '@/features/auth/session';
+import { serialize } from 'cookie';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 function logoutRoute(
@@ -9,8 +10,18 @@ function logoutRoute(
   console.log('logout before', req.session);
   res.setHeader('Cache-Control', 'no-cache');
   req.session.destroy();
+
+  // cookie を明示的に削除
+  const cookieName = process.env.NEXT_PUBLIC_APP_SITE_NAME as string;
+  res.setHeader('Set-Cookie', [
+    serialize(cookieName, '', {
+      maxAge: -1,
+      path: '/',
+    }),
+  ]);
   console.log('logout after', req.session);
-  res.json({ ok: true });
+
+  res.send({ ok: true });
 }
 
 export default withIronSessionApiRoute(logoutRoute, sessionOptions);
